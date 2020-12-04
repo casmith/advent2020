3ecbcfc04f9bd87135cf548979338db11fec0687
Clay Smith <uhclay@gmail.com>
2020-12-03 18:40:36 -0800

    Rename the rest of day 2

--- advent2-2.js
+++ advent2-2.js
@@ -1,7 +1,7 @@
 const readfile = require('./readfile')
 const check = rule => !!((rule.password[rule.rule[0]] === rule.char) ^ (rule.password[rule.rule[1]] === rule.char));
 
-const rules = readfile('day3-4.txt')
+const rules = readfile('day2.txt')
 	.map(line => line.split(" "))
 	.map(a => ({rule: a[0].split("-").map(Number).map(n => n - 1), char: a[1][0], password: a[2]}));
 	
